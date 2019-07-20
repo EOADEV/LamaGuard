1444773ef92cd3e21c9244ec0c90dbdb9a8ebf70
EOA <eoa.dev@gmail.com>
2019-07-20 12:35:43 +0300

    radar code ready

--- radarCodeTest/radarCodeTest.ino
+++ radarCodeTest/radarCodeTest.ino
@@ -1,11 +1,24 @@
 #define RADAR_PIN 3
+#define BUZZER_PIN 12
+#define LED_PIN 13
+bool radarState;
 
 void setup() {
   Serial.begin(9600);
   pinMode(RADAR_PIN, INPUT);
+  Serial.println("Radar test");
 }
 
 void loop() {
-  Serial.println(digitalRead(RADAR_PIN));
+  radarState = digitalRead(RADAR_PIN);
+  Serial.println(radarState);
+  if(radarState){
+    tone(BUZZER_PIN,100);
+    digitalWrite(LED_PIN,HIGH);
+  }
+  else{
+    noTone(BUZZER_PIN);
+    digitalWrite(LED_PIN,LOW);
+  }
   delay(50);
 }
